cb10f5d4f9508f54afba7ebd758669119a466b2a
Raquel <anaraquelbpereira@gmail.com>
2025-11-06 21:42:36 -0300

    tratamento de evento: Filtragem dos animais por tipo

--- miau_dota-app/src/main.js
+++ miau_dota-app/src/main.js
@@ -1,59 +1,72 @@
-import './style.css'
-import pets from './pets'
+import "./style.css";
+import pets from "./pets";
 
-function createPet(pets){
-    return `
+function createPet(pets) {
+  return `
     <div>
         <img src="${pets.image}">
         <p class="name">${pets.name}</p>
         <p class="infos-pet">${pets.age} | ${pets.sexo}</p>
         <img src="src/imgs/Itens miaudota/pata1.png" alt="pata1" id="pata1">
-    </div>`
-           
-};
+    </div>`;
+}
 
-const petcard = document.querySelector('.pets');
+const petcard = document.querySelector(".pets");
 
 pets.forEach((pets) => {
-    let card = createPet(pets);
-    petcard.insertAdjacentHTML('beforeend', card);
+  let card = createPet(pets);
+  petcard.insertAdjacentHTML("beforeend", card);
 });
 
-const btnSexo = document.getElementById('btnSexo');
-const opcoesSexo = document.getElementById('opcoesSexo');
-const btnFemea = document.getElementById('btnFemea');
-const btnMacho = document.getElementById('btnMacho');
-
-btnSexo.addEventListener('mouseover', event => {
-    opcoesSexo.style.display = 'flex';
-    btnFemea.style.backgroundColor = '#FCF9F3';
-    btnFemea.style.color = '#452824';
-    btnMacho.style.backgroundColor = '#FCF9F3';
-    btnMacho.style.color = '#452824';
-});
-
-dropdownSexo.addEventListener('mouseover', event => {
-    opcoesSexo.style.display = 'flex';
-});
-
-opcoesSexo.addEventListener('mouseleave', event => {
-    opcoesSexo.style.display = 'none';
-});
-
+// Filtragem dos pets
 function filtrarPets(filtro, opcao) {
-    petcard.innerHTML = '';
-    const filtrados = pets.filter(pet => pet[filtro] === opcao);
-    filtrados.forEach(pet => {
-        petcard.innerHTML += createPet(pet);
-    });
-};
+  petcard.innerHTML = "";
+  const filtrados = pets.filter((pet) => pet[filtro] === opcao);
+  filtrados.forEach((pet) => {
+    petcard.innerHTML += createPet(pet);
+  });
+}
 
-btnFemea.addEventListener('click', event => {
-    filtrarPets('sexo', 'femea');
-    btnFemea.style.color = '#e58233';
+//Tratamento de eventos do botão Sexo
+const btnSexo = document.getElementById("btnSexo");
+const opcoesSexo = document.getElementById("opcoesSexo");
+const btnFemea = document.getElementById("btnFemea");
+const btnMacho = document.getElementById("btnMacho");
+
+btnSexo.addEventListener("mouseover", (event) => {
+  opcoesSexo.style.display = "flex";
+  btnFemea.style.backgroundColor = "#FCF9F3";
+  btnFemea.style.color = "#452824";
+  btnMacho.style.backgroundColor = "#FCF9F3";
+  btnMacho.style.color = "#452824";
 });
 
-btnMacho.addEventListener('click', event => {
-    filtrarPets('sexo', 'macho');
-    btnMacho.style.color = '#e58233';
+dropdownSexo.addEventListener("mouseover", (event) => {
+  opcoesSexo.style.display = "flex";
+});
+
+opcoesSexo.addEventListener("mouseleave", (event) => {
+  opcoesSexo.style.display = "none";
+});
+
+// Filtragem dos pets por sexo
+btnFemea.addEventListener("click", (event) => {
+  filtrarPets("sexo", "femea");
+  btnFemea.style.color = "#e58233";
+});
+
+btnMacho.addEventListener("click", (event) => {
+  filtrarPets("sexo", "macho");
+  btnMacho.style.color = "#e58233";
+});
+
+//Tratamento de evento do botão Miau
+const btnMiau = document.getElementById("btnMiau");
+
+btnMiau.addEventListener("click", (event) => {
+  filtrarPets("type", "cat");
+});
+
+btnAuau.addEventListener("click", event => {
+    filtrarPets("type", "dog");
 });
