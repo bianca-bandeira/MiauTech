dcdcf8b9ddc9142da7ba50144a8c414eb4806f00
pprtbia <biancarb1421@gmail.com>
2025-11-07 00:24:33 -0300

    corrigir deploy do CSS

--- miau_dota-app/vite.config.js
+++ miau_dota-app/vite.config.js
@@ -2,11 +2,4 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: '/Miau-dota/',
-    build: {
-        rollupOptions: {
-            input: {
-                main: 'index.html',
-            }
-        }
-    }
 })
